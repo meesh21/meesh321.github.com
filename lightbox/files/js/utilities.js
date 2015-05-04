36d08f707a4bc110dd0214bd32eac0030f50a70c
meesh21 <michelle.hayes@rutgers.edu>
2015-05-04 11:04:14 -0400

    update lightbox

---
+++ lightbox/files/js/utilities.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $('.project').magnificPopup({
+    type: 'image'
+  });
+
+  $('.gallery').magnificPopup({
+    type: 'image',
+    gallery:{
+      enabled:true
+    }
+  });
+
+});
+
+    Status API Training Shop Blog About 
+
